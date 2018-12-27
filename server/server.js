6ca142f31b378e3eea27bc14197d05dbaf85b2c6
shanjo <mike_b_sorensen@live.dk>
2018-12-27 16:29:00 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -70,7 +70,7 @@ app.use(function(req, res, next) {
     }
   }
 });
-
+console.log(process.argv);
 if (process.argv.length <= 2) {
   console.log("Usage: " + __filename + " path/to/directory");
   process.exit(-1);
