8d8ea651cae1426c47819de0b9b15fa3c25d0d48
shanjo <mike_b_sorensen@live.dk>
2019-01-02 20:47:50 +0100

    Update edit_agent.js
    Minor bugfix @ edit_agents:49

--- web/src/app/components/agents/edit_agent.js
+++ web/src/app/components/agents/edit_agent.js
@@ -46,7 +46,7 @@ function EditAgentController($rootScope,$scope, Agent, Intents, Entities,AgentEn
 
   $scope.combineToAgent = function() {
     console.log('Call Was a success');
-    Agent.combineAgents({agent_id: $scope.$routeParams.agent_id, combine_with: 2,4}).$promise.then(function(resp) {
+    Agent.combineAgents({agent_id: $scope.$routeParams.agent_id, combine_with: '2,4'}).$promise.then(function(resp) {
       console.log('Combined');
     });
   };
