5a02e180fd80d0f9be8f070a5a78487c5a14b493
shanjo <mike_b_sorensen@live.dk>
2019-01-02 22:54:58 +0100

    Update edit_agent.js

--- web/src/app/components/agents/edit_agent.js
+++ web/src/app/components/agents/edit_agent.js
@@ -44,9 +44,7 @@ function EditAgentController($rootScope,$scope, Agent, Intents, Entities,AgentEn
   };
 
   $scope.combineToAgent = function(agent) {
-    console.log(agent);
-    var form = {};
-    form.combine_with = '2,4';
+    agent.combine_with = '2,4';
     Agent.update({ agent_id:$scope.$routeParams.agent_id }, agent).$promise.then(function() {
       $rootScope.$broadcast('setAlertText', "Combined!!");
     });
