1c8c4d39fea81da59e81c9c4868ac6c764d52f19
shanjo <mike_b_sorensen@live.dk>
2019-01-03 18:05:44 +0100

    MIQ UI: account
    Update of structure to fit the current structure in DB

--- server/db/accounts.js
+++ server/db/accounts.js
@@ -1,7 +1,7 @@
 const db = require('./db')
 
 function getAccounts(req, res, next) {
-  db.one('select * from account')
+  db.one('select * from account as a JOIN account_type as at ON at.id = a.account_type')
     .then(function (data) {
       res.status(200)
         .json(data);
