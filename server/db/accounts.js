d6e0b81cfb184a8525685b4755ad2d690ab5e3b2
shanjo <mike_b_sorensen@live.dk>
2019-01-03 13:34:37 +0100

    Update accounts.js

--- server/db/accounts.js
+++ server/db/accounts.js
@@ -3,9 +3,11 @@ const db = require('./db')
 function getAccounts(req, res, next) {
   db.one('select * from account')
     .then(function (data) {
-      var data = Object.entries(data).map((value)=>(value));
       res.status(200)
-        .json(data);
+        .json({
+          'message': 'hej',
+          'test' : true,
+        });
     })
     .catch(function (err) {
       return next(err);
