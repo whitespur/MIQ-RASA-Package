f153c3f5ed5881b21e152d870a68cf89b7d3f3ec
shanjo <mike_b_sorensen@live.dk>
2019-01-03 12:19:45 +0100

    Update router.js

--- web/src/app/router.js
+++ web/src/app/router.js
@@ -116,7 +116,7 @@ app.config(function($routeProvider, $locationProvider) {
       activePage:'settings'
     })
     .when('/accounts', {
-      controller:'SettingsController',
+      controller:'AccountsController',
       templateUrl:'/app/components/accounts/accounts.html',
       activePage:'accounts'
     })
