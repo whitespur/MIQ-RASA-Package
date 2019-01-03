26e3462bdec972e4b25ee25ad9f4c32872025f8f
shanjo <mike_b_sorensen@live.dk>
2019-01-03 20:42:47 +0100

    Update accounts.js

--- web/src/app/components/accounts/accounts.js
+++ web/src/app/components/accounts/accounts.js
@@ -3,7 +3,7 @@ angular
 .controller('AccountsController', AccountsController)
 
 function AccountsController($scope, $rootScope, Account) {
-  Account.query(function(data) {
+  Account.get(function(data) {
     if(data.username !== undefined) {
       data = [data];
     }
