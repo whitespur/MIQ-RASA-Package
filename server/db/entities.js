45f559c3d58ad3736c3e02a07351f4bfa7baf75b
shanjo <mike_b_sorensen@live.dk>
2019-01-03 09:22:40 +0100

    Update entities.js

--- server/db/entities.js
+++ server/db/entities.js
@@ -15,7 +15,13 @@ function getAllEntities(req, res, next) {
 function getAllEntitiesForAgent(req, res, next) {
   console.log("Entities.getAllEntitiesForAgent");
   var agentId = parseInt(req.params.agent_id);
-  var IDS = req.query.combined_to + ',' + agent_id;
+  var CombinedIds = req.query.combined_to;
+  var IDS;
+  if(CombinedIds != undefined ) {
+    IDS = CombinedIds + ',' + AgentID;
+  } else {
+    IDS = AgentID;
+  }
   db.any('select * from entities where agent_id IN (' + IDS + ')')
     .then(function (data) {
       res.status(200)
