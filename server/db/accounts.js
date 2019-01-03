d17b99e2b1b096a575033050bfb19e09d09a28b1
shanjo <mike_b_sorensen@live.dk>
2019-01-03 20:25:46 +0100

    Update accounts.js

--- server/db/accounts.js
+++ server/db/accounts.js
@@ -1,7 +1,7 @@
 const db = require('./db')
 
 function getAccounts(req, res, next) {
-  db.one('select username, email, client_id, created_on, name as account_type_name from account JOIN account_type ON account_type.id = account.account_type')
+  db.one('select username, email, client_id, created_on, name as account_type_name from account')
     .then(function (data) {
       res.status(200)
         .json(data);
