1d50476ba6b427f543b4aea6946f693630322482
shanjo <mike_b_sorensen@live.dk>
2019-01-10 13:51:48 +0100

    Update edit_agent.js

--- web/src/app/components/agents/edit_agent.js
+++ web/src/app/components/agents/edit_agent.js
@@ -44,7 +44,7 @@ function EditAgentController($rootScope,$scope, Agent, Intents, Entities,AgentEn
 
   $scope.setDefaultCallback = function(agent, text) {
     console.log(text);
-    agent.fallback_content = "" + text + "";
+    agent.default_text = "" + text + "";
     Agent.update({ agent_id:agent.agent_id }, agent).$promise.then(function(response) {
         $rootScope.$broadcast('setAlertText', "Default fallback updated!!");
     });
