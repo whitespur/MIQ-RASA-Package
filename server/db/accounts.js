ddd6669f65b3a1b6a1142be0bc9bf1f9a55af984
shanjo <mike_b_sorensen@live.dk>
2019-01-14 14:41:41 +0100

    Update accounts.js

--- server/db/accounts.js
+++ server/db/accounts.js
@@ -13,8 +13,10 @@ function getAccounts(req, res, next) {
 
 function getSingleAccount(req, res, next) {
   var accountID = parseInt(req.params.accounts_id);
+  console.log(req.params.accounts_id);
   db.one('select * from account where user_id = $1', accountID)
     .then(function (data) {
+      console.log(data);
       res.status(200)
         .json(data);
     }).catch(function (err) {
