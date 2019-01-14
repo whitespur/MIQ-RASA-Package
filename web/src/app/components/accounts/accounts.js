a8103a7c5839d97ab4f06feddbdf1ba3b19380aa
shanjo <mike_b_sorensen@live.dk>
2019-01-14 13:48:27 +0100

    Update accounts.js

--- web/src/app/components/accounts/accounts.js
+++ web/src/app/components/accounts/accounts.js
@@ -3,6 +3,7 @@ angular
 .controller('AccountsController', AccountsController)
 
 function AccountsController($scope, $rootScope, Account) {
+  console.log(Account);
   Account.query(function(data) {
     $scope.accountList = data;
   });
