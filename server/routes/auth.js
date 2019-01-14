41b1ad77c0f02df9f4a7f5a443e140c15af626a4
shanjo <mike_b_sorensen@live.dk>
2019-01-14 10:55:31 +0100

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -23,7 +23,7 @@ var pages = {
   10:'permission_center'
 };
 
-  onAuthenticate = function(req) {
+  onAuthenticate = function(req, res, next) {
       //authenticate user
       console.log("Authenticate User");
       db.one('select * from account where username = $1', [req.body.username])
