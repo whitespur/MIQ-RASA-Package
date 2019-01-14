65e097c7a36f063159f8f7743014c36703e24912
shanjo <mike_b_sorensen@live.dk>
2019-01-14 14:24:26 +0100

    Update sidebar.js

--- web/src/app/shared/sidebar/sidebar.js
+++ web/src/app/shared/sidebar/sidebar.js
@@ -5,8 +5,10 @@ angular
 function SideBarController(Navigation, Account) {
     Account.get({account_id: $scope.$routeParams.account_id}, function(data) {
         $scope.account = data;
+        console.log(data);
         Navigation.get({level: data.level}, function(response) {
             $scope.nav_links = response;
+            console.log(response);        
         });
     });
 }
