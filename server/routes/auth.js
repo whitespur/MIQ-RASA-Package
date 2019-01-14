91b6c46d9ce86f3de87c758915af2775427e8599
shanjo <mike_b_sorensen@live.dk>
2019-01-14 23:49:01 +0100

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -24,7 +24,7 @@ var pages = {
   10:'permission_center'
 };
 
-var components = ['navigation','accounts', 'avgNluResponseTimesLast30Days', 'activeUserCountLast30Days'];
+var components = ['navigation','accounts', 'avgNluResponseTimesLast30Days', 'activeUserCountLast30Days', 'agentsByIntentConfidencePct'];
 
   onAuthenticate = function(req, res, next) {
       //authenticate user
