3923e2550705e018abde23d5b3b2da3fc850adb2
shanjo <mike_b_sorensen@live.dk>
2019-01-14 20:26:00 +0100

    Update sidebar.js

--- web/src/app/shared/sidebar/sidebar.js
+++ web/src/app/shared/sidebar/sidebar.js
@@ -2,7 +2,7 @@ angular
 .module('app')
 .controller('SideBarController', SideBarController)
 
-function SideBarController($scope,Navigation, Account) {
+function SideBarController($scope,$http, Account) {
     Account.get({account_id: $scope.$routeParams.account_id}, function(data) {
         $scope.account = data;
         console.log(data);
