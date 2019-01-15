fb71593b86196a9da2572cba403335b836a853db
shanjo <mike_b_sorensen@live.dk>
2019-01-15 14:03:38 +0100

    Revert "Update aside.js"
    This reverts commit bd970e9bbc8923414c90769921cca099d8d5f3d8.

--- web/src/app/shared/aside/aside.js
+++ web/src/app/shared/aside/aside.js
@@ -69,8 +69,11 @@ function AsideController($scope, $rootScope, $interval, Agent, Account, $http,Ra
       Rasa_Config.get().$promise.then(function(data) {
         $rootScope.config = data.toJSON();
         $rootScope.config.isonline = 1;
-        console.log(agent_names);
-        $rootScope.config.server_model_dirs_array = getAvailableModels(statusdata, agent_names, $scope.account.level);
+        if(agent_names !== undefined) {
+          $rootScope.config.server_model_dirs_array = getAvailableModels(statusdata, agent_names, $scope.account.level);
+        } else {
+          $rootScope.config.server_model_dirs_array = getAvailableModels(statusdata);
+        }
         if ($rootScope.config.server_model_dirs_array.length > 0) {
             $rootScope.modelname = $rootScope.config.server_model_dirs_array[0].name;
         }
