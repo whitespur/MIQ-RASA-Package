69f190df862cf118ba86020686925243a50717e6
shanjo <mike_b_sorensen@live.dk>
2019-01-15 12:19:02 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -130,7 +130,8 @@ if (app.get('env') === 'development') {
     res.status( err.code || 500 )
     .json({
       status: 'error',
-      message: err
+      message: err,
+      req:req
     });
   });
 }
