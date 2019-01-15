ce7428e8dceaa5af39e601620ee89f90abc53cff
shanjo <mike_b_sorensen@live.dk>
2019-01-15 09:17:54 +0100

    Update edit_account.js

--- web/src/app/components/accounts/edit_account.js
+++ web/src/app/components/accounts/edit_account.js
@@ -2,7 +2,7 @@ angular
 .module('app')
 .controller('EditAccountController', EditAccountController)
 
-function EditAccountController($scope, $rootScope, Account) {
+function EditAccountController($scope, $rootScope, Account, $sessionStorage) {
   Account.get({account_id: $scope.$routeParams.account_id}, function(data) {
       $scope.account = data;
   });
