c968db180087a3a2c8f614a1509e482b9df1189d
shanjo <mike_b_sorensen@live.dk>
2019-01-24 12:15:38 +0100

    Update edit_intent.js

--- web/src/app/components/intents/edit_intent.js
+++ web/src/app/components/intents/edit_intent.js
@@ -33,8 +33,7 @@ function EditIntentController($rootScope, $scope, Agent, AgentEntities, Intent, 
   $scope.saveNewResponse = function(event){
     this.formData.intent_id = $scope.$routeParams.intent_id;
     this.formData.response_type = 1;//DEFAULT type
-    this.formData.response_text = this.formData.response_text.replace(/(?:\r\n|\r|\n)/g, '<br>');
-    console.log(test);
+    this.formData.response_text = this.formData.response_text.replace(/(?:\r\n|\r|\n)/g, '</br>');
     Response.save(this.formData).$promise.then(function(resp) {
       //update list
       loadResponses();
