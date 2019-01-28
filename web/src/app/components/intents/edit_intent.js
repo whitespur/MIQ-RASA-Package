7db25aa9dc377ea2fdf299ae5f98e81f6b47a23e
shanjo <mike_b_sorensen@live.dk>
2019-01-28 14:27:26 +0100

    Intents

--- web/src/app/components/intents/edit_intent.js
+++ web/src/app/components/intents/edit_intent.js
@@ -51,6 +51,7 @@ function EditIntentController($rootScope, $scope, Agent, AgentEntities, Intent, 
   $scope.showTextTaskbar = function(ev) {
     var start = ev.currentTarget.selectionStart;
     var end   = ev.currentTarget.selectionEnd;
+    console.log(ev.currentTarget);
     var text = ev.currentTarget.value.substr(start, end - start);
     if(text !== '') {
       $scope.text_selected = text;
