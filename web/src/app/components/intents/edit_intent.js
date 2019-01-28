967a2cc476175de3b2d6f51a11172fbe49b27d49
shanjo <mike_b_sorensen@live.dk>
2019-01-28 09:19:21 +0100

    Update edit_intent.js

--- web/src/app/components/intents/edit_intent.js
+++ web/src/app/components/intents/edit_intent.js
@@ -49,7 +49,13 @@ function EditIntentController($rootScope, $scope, Agent, AgentEntities, Intent, 
   }
 
   $scope.showTextTaskbar = function(e) {
-    console.log(e);
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    this.selectedText = text;
   }
 
   $scope.updateIntentNameAndWebhook = function(intent) {
