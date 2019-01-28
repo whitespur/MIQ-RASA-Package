e1f16011356ab0433fc944c5becad5ded2db7016
shanjo <mike_b_sorensen@live.dk>
2019-01-28 10:57:53 +0100

    Update edit_intent.js

--- web/src/app/components/intents/edit_intent.js
+++ web/src/app/components/intents/edit_intent.js
@@ -52,7 +52,11 @@ function EditIntentController($rootScope, $scope, Agent, AgentEntities, Intent, 
     var start = ev.currentTarget.selectionStart;
     var end   = ev.currentTarget.selectionEnd;
     var text = ev.currentTarget.value.substr(start, end - start);
-  console.log(text);
+  console.log(ev.currentTarget.value);
+  console.log(start);
+  console.log(end);
+
+
   }
 
   $scope.updateIntentNameAndWebhook = function(intent) {
