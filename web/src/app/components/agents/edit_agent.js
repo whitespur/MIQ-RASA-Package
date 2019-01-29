9aed7d105e7f0bc34963bc9e9ca1f11a09b445e9
shanjo <mike_b_sorensen@live.dk>
2019-01-29 13:52:04 +0100

    Update edit_agent.js

--- web/src/app/components/agents/edit_agent.js
+++ web/src/app/components/agents/edit_agent.js
@@ -20,14 +20,15 @@ function EditAgentController($rootScope,$scope, Agent, Intents, Entities,AgentEn
       }
 
       for(var i in data[1]) {
-        var id = data[1][i].intent_name;
-        if(obj[id] !== undefined) {
+        var id = data[1][i].intent_id;
+        var name = data[1][i].intent_name;
+        if(data[1][id] !== undefined) {
           var res_id = data[1][i].response_id;
-          if(typeof(obj[id]['responses']) !== 'object') {
-            obj[id]['responses'] = {};
+          if(typeof(obj[name]['responses']) !== 'object') {
+            obj[name]['responses'] = {};
           }
-          obj[id]['responses'][res_id] = data[1][i];
-          obj[id]['response_count']++;
+          obj[name]['responses'][res_id] = data[1][i];
+          obj[name]['response_count']++;
         }
       }
       console.log(obj);
