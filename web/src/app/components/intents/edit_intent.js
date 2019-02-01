45b0d964905f6c38c0a8422ce7cd7d9d9f1aeb41
shanjo <mike_b_sorensen@live.dk>
2019-02-01 10:40:03 +0100

    Update edit_intent.js

--- web/src/app/components/intents/edit_intent.js
+++ web/src/app/components/intents/edit_intent.js
@@ -65,6 +65,7 @@ function EditIntentController($rootScope, $scope, Agent, AgentEntities, Intent, 
     console.log($scope.active_tag);
     if($scope.active_tag === 'create') {
       $scope.last_tag = $scope.active_tag;
+      console.log('i Create Stuff');
     } else if($scope.last_tag !== $scope.active_tag && $scope.last_tag !== '') {
       if($scope.tagsInNames.indexOf($scope.tagList[$scope.last_tag].category_name) !== -1) {
         $scope.tagsInNames = $scope.tagsInNames.replace('<span>' + $scope.tagList[$scope.last_tag].category_name + '</span>', '');
