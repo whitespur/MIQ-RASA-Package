3c3a8ab3b361c2df4fce74216c6c435d062cae40
shanjo <mike_b_sorensen@live.dk>
2019-02-01 10:43:01 +0100

    Update edit_intent.js

--- web/src/app/components/intents/edit_intent.js
+++ web/src/app/components/intents/edit_intent.js
@@ -68,9 +68,9 @@ function EditIntentController($rootScope, $scope, Agent, AgentEntities, Intent, 
       console.log('i Create Stuff');
     } else if($scope.last_tag !== $scope.active_tag && $scope.last_tag !== '') {
       if($scope.tagsInNames.indexOf($scope.tagList[$scope.last_tag].category_name) !== -1) {
-        $scope.tagsInNames = $scope.tagsInNames.replace('<span>' + $scope.tagList[$scope.last_tag].category_name + '</span>', '');
+        $scope.tagsInNames = $scope.tagsInNames.replace('<span>' + $scope.tagList[$scope.active_tag].category_name + '</span>', '');
       } else {
-        $scope.tagsInNames = $scope.tagsInNames + '<span>' + $scope.tagList[$scope.last_tag].category_name + '</span>';
+        $scope.tagsInNames = $scope.tagsInNames + '<span>' + $scope.tagList[$scope.active_tag].category_name + '</span>';
       }
       $scope.last_tag = $scope.active_tag;
     } else {
