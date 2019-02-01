70b0ee1984a697b9794684d2332ba66d94aa7a84
shanjo <mike_b_sorensen@live.dk>
2019-02-01 13:36:11 +0100

    Update edit_intent.js

--- web/src/app/components/intents/edit_intent.js
+++ web/src/app/components/intents/edit_intent.js
@@ -27,7 +27,7 @@ function EditIntentController($rootScope, $scope, Agent, AgentEntities, Intent, 
 
   Bind.query({intent_id: $scope.$routeParams.intent_id},function(data) {
       angular.forEach(data, function(value) {
-          $scope.tagsInNames = $scope.tagsInNames + '<span>' + value.category_name + '<span ng-click="removeTag(' + value.category_name + ')">x</span></span>';
+          $scope.tagsInNames = $scope.tagsInNames + '<span>' + value.category_name + '<span ng-click="removeTag($event)">x</span></span>';
         confirm.log($scope.tagsInNames);
         });
   });
