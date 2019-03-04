f1b9c1c09ccae3f139e69d4632efdf98c78cf608
shanjo <mike_b_sorensen@live.dk>
2019-03-04 04:04:24 +0100

    intents

--- web/src/app/components/intents/edit_intent.js
+++ web/src/app/components/intents/edit_intent.js
@@ -165,12 +165,12 @@ $('#response_text').on('focus', function() {
     if(v == '') {
       v = 'Insert your text block data here';
     }
-    parent.find('.text-block-container').append('<div class="single-block"><textarea>' + v + '</textarea></div>');
+    parent.find('.text-block-container').append('<div class="single-block"><div class="innerTaskbar"><a>X</a></div><textarea>' + v + '</textarea></div>');
   })
 });
 
 $scope.addTextSection = function(e) {
-  IntentTextBlockContainer.append('<div class="single-block"><textarea>Insert your text block data here</textarea></div>')
+  IntentTextBlockContainer.append('<div class="single-block"><div class="innerTaskbar"><a>X</a></div></div><textarea>Insert your text block data here</textarea></div>')
   IntentTextBlockContainer.find('.single-block:last-child').trigger('click');
 }
 
